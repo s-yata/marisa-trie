e4d5dc29bf717cdf3bdd6168576437c7884e1c00
Gleb Mazovetskiy <glex.spb@gmail.com>
2025-05-31 10:29:24 +0100

    Use `uint32_t` instead of `size_t` where possible
    Marisa node IDs are 32-bit integers, so using a 64-bit `size_t`
    for nodes and related types wastes memory.
    Additionally, key and query lengths etc are all effectively
    limited to 32-bit.
    
    Replaces `size_t` with `uint32_t` where appropriate.
    This reduces the sizes of various structs throughout.
    
    Also replaces the remaining `marisa::UInt*` usages with `<cstdint>`
    types. I've opted to not use the `std::` prefix for `<cstdint>`
    types because it is verbose and not required.

--- include/marisa/keyset.h
+++ include/marisa/keyset.h
@@ -40,17 +40,17 @@ class Keyset {
     return key_blocks_[i / KEY_BLOCK_SIZE][i % KEY_BLOCK_SIZE];
   }
 
-  std::size_t num_keys() const {
+  uint32_t num_keys() const {
     return size_;
   }
 
   bool empty() const {
     return size_ == 0;
   }
-  std::size_t size() const {
+  uint32_t size() const {
     return size_;
   }
-  std::size_t total_length() const {
+  uint32_t total_length() const {
     return total_length_;
   }
 
@@ -61,23 +61,23 @@ class Keyset {
 
  private:
   std::unique_ptr<std::unique_ptr<char[]>[]> base_blocks_;
-  std::size_t base_blocks_size_ = 0;
-  std::size_t base_blocks_capacity_ = 0;
+  uint32_t base_blocks_size_ = 0;
+  uint32_t base_blocks_capacity_ = 0;
   std::unique_ptr<std::unique_ptr<char[]>[]> extra_blocks_;
-  std::size_t extra_blocks_size_ = 0;
-  std::size_t extra_blocks_capacity_ = 0;
+  uint32_t extra_blocks_size_ = 0;
+  uint32_t extra_blocks_capacity_ = 0;
   std::unique_ptr<std::unique_ptr<Key[]>[]> key_blocks_;
-  std::size_t key_blocks_size_ = 0;
-  std::size_t key_blocks_capacity_ = 0;
+  uint32_t key_blocks_size_ = 0;
+  uint32_t key_blocks_capacity_ = 0;
   char *ptr_ = nullptr;
-  std::size_t avail_ = 0;
-  std::size_t size_ = 0;
-  std::size_t total_length_ = 0;
+  uint32_t avail_ = 0;
+  uint32_t size_ = 0;
+  uint32_t total_length_ = 0;
 
-  char *reserve(std::size_t size);
+  char *reserve(uint32_t size);
 
   void append_base_block();
-  void append_extra_block(std::size_t size);
+  void append_extra_block(uint32_t size);
   void append_key_block();
 };
 
